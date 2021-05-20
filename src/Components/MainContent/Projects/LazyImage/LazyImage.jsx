ea01afbb45689df82f89e70e017c5222ba1a5e3e
Bruno <bruno.fdm93@gmail.com>
2021-05-20 00:04:59 -0700

    better ui

--- src/Components/MainContent/Projects/LazyImage/LazyImage.jsx
+++ src/Components/MainContent/Projects/LazyImage/LazyImage.jsx
@@ -1,34 +1,47 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 
 const LazyImage = ({ width, height, src, ...rest }) => {
-  const [ref, inView] = useInView({
+  const wrapper = useRef();
+  const [ref, inView, entry] = useInView({
+    threshold: [0, 0.33, 0.66, 1],
     triggerOnce: true,
-    rootMargin: "0px 0px",
+    trackVisibility: true,
+    delay: 100,
+    rootMargin: "300px",
   });
 
   return (
     <div
       ref={ref}
-      data-inview={inView}
       style={{
-        position: "relative",
-        maxWidth: "440px",
-        marginBottom: 35,
-        maxHeight: height,
+        width: "100%",
+        display: "flex",
+        justifyContent: "center",
+        // background: inView ? "#332f2f" : "#000",
       }}
     >
-      {inView && (
-        <motion.img
-          initial={{ opacity: 0}}
-          animate={{ opacity: 1}}
-          transition={{ type: "spring", duration: 1.3, delay: 0.3 }}
-          {...rest}
-          src={src}
-          style={{ maxWidth: "100%" }}
-        />
-      )}
+      <div
+        data-inview={inView}
+        style={{
+          position: "relative",
+          minHeight: height / 1.2,
+          paddingTop: `${(height / width) * 12}%`,
+          paddingBottom: `${(height / width) * 12}%`,
+        }}
+      >
+        {inView && (
+          <motion.img
+            initial={{ opacity: 0 }}
+            animate={{ opacity: inView ? 1 : 0 }}
+            transition={{ type: "spring", duration: 1.3, delay: 0.23 }}
+            {...rest}
+            src={src}
+            style={{ maxWidth: "440px", height: "auto" }}
+          />
+        )}
+      </div>
     </div>
   );
 };
