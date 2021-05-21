347ceaf4799d66f9596ab367bd083bfdd6912828
Bruno <bruno.fdm93@gmail.com>
2021-05-21 12:11:29 -0700

    projects in gif stable, before mp4

--- src/Components/MainContent/Projects/LazyProject/LazyProject.jsx
+++ src/Components/MainContent/Projects/LazyProject/LazyProject.jsx
@@ -2,13 +2,21 @@ import React, { useState, useEffect, useRef } from "react";
 import { useInView } from "react-intersection-observer";
 import { motion } from "framer-motion";
 import { makeStyles } from "@material-ui/core/styles";
+import { useTheme } from "@material-ui/core/styles";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+import { Typography } from "@material-ui/core";
 
 const LazyProject = ({ width, height, src, ...rest }) => {
   const wrapper = useRef();
   const [hasShown, setHasShown] = useState(false);
+  const theme = useTheme();
+
+  const isXs = useMediaQuery(theme.breakpoints.down("sm"));
+
   const { ref, inView, entry } = useInView({
     threshold: [0.2, 0.4],
-    rootMargin: "-500px 0px -100px 0px",
+    rootMargin: "-450px 0px -300px 0px",
+
     // trackVisibility: true,
     // delay: 100,
     // rootMargin: "300px",
@@ -48,11 +56,29 @@ const LazyProject = ({ width, height, src, ...rest }) => {
 
   const classes = useStyles();
 
+  const variants = {
+    appear: {
+      y: !hasShown || isXs ? 140 : 0,
+      transition: {
+        type: "anticipate",
+        duration: 1,
+      },
+    },
+    changeBg: {
+      backgroundColor: inView ? "#090909" : "#000",
+      // paddingTop: inView ? "100px" : 0,
+      // paddingBottom: inView ? "100px" : 0,
+      transition: {
+        delay: hasShown ? 0.05 : 0.25,
+        type: "anticipate",
+        duration: 0.4,
+      },
+    },
+  };
+
   useEffect(() => {
     if (!hasShown && inView) {
-      setTimeout(() => {
-        setHasShown(true);
-      }, 200);
+      setHasShown(true);
     }
   }, [inView]);
 
@@ -63,41 +89,45 @@ const LazyProject = ({ width, height, src, ...rest }) => {
         width: "100%",
         display: "flex",
         justifyContent: "center",
-        marginBottom: "80px",
+        alignItems: "center",
+        marginBottom: !isXs ? "55px" : "10px",
+        minHeight: !isXs && "530px",
+        flexDirection: "column",
       }}
-      initial={{ y: !hasShown ? 50 : 0 }}
-      animate={{
-        backgroundColor: inView ? "#090909" : "#000",
-        paddingTop: inView ? "60px" : 0,
-        paddingBottom: inView ? "60px" : 0,
-        y: !hasShown ? 50 : 0,
-      }}
-      transition={{ type: "anticipate", duration: 0.4, delay: 0.1 }}
+      variants={variants}
+      initial={{ y: !hasShown ? 140 : 0 }}
+      animate={["appear", "changeBg"]}
     >
+      <div style={{ width: "100%" }}>
+        <Typography color="primary">
+          <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+            work for COMPANY NAME
+          </motion.span>{" "}
+          <br />
+          <motion.span> Description of the work... ... ...</motion.span>
+        </Typography>
+      </div>
       <div
         data-inview={inView}
-        // className={classes.loadingImg}
         style={{
           position: "relative",
           minHeight: height / 1.2,
-          background: hasShown ? "" : "",
-          maxWidth: "440px",
           width: "100%",
+          maxWidth: "440px",
         }}
       >
         {hasShown && (
           <motion.img
-            initial={{ opacity: 0.5 }}
-            animate={{ opacity: inView ? 1 : 0.5 }}
+            initial={{ opacity: 0.2 }}
+            animate={{ opacity: inView ? 1 : 0.1 }}
             exit={{ opacity: 0 }}
-            transition={{ type: "spring", duration: 1.3 }}
+            transition={{ type: "anticipate", duration: 0.8 }}
             {...rest}
             src={src}
             style={{
               maxWidth: "440px",
+              width: "100%",
               height: "auto",
-              paddingTop: `${(height / width) * 12}%`,
-              paddingBottom: `${(height / width) * 12}%`,
             }}
           />
         )}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./Components/App";
+import "./Components/App.css";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 
@@ -10,6 +11,22 @@ const theme = createMuiTheme({
       main: "#fff",
       contrastText: "#000",
     },
+  },
+  typography: {
+    fontFamily: "Oxanium, cursive",
+    //  [
+    //   "Oxanium, cursive",
+    //   "-apple-system",
+    //   "BlinkMacSystemFont",
+    //   '"Segoe UI"',
+    //   "Roboto",
+    //   '"Helvetica Neue"',
+    //   "Arial",
+    //   "sans-serif",
+    //   '"Apple Color Emoji"',
+    //   '"Segoe UI Emoji"',
+    //   '"Segoe UI Symbol"',
+    // ].join(","),
   },
 });
 
