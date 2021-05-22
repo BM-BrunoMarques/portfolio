2b7d71f128cf4d4fc7037fc95b8765bbeccd6410
Bruno <bruno.fdm93@gmail.com>
2021-05-22 12:55:17 -0700

    working with mp4 projects

--- src/Components/MainContent/Projects/LazyProject/LazyProject.jsx
+++ src/Components/MainContent/Projects/LazyProject/LazyProject.jsx
@@ -5,60 +5,67 @@ import { makeStyles } from "@material-ui/core/styles";
 import { useTheme } from "@material-ui/core/styles";
 import useMediaQuery from "@material-ui/core/useMediaQuery";
 import { Typography } from "@material-ui/core";
+import ReactPlayer from "react-player";
+
+import ddbc from "../mp4/ddbc.mp4";
 
 const LazyProject = ({ width, height, src, ...rest }) => {
   const wrapper = useRef();
   const [hasShown, setHasShown] = useState(false);
   const theme = useTheme();
 
+  const [isPlay, setIsPlay] = useState(false);
+
   const isXs = useMediaQuery(theme.breakpoints.down("sm"));
 
   const { ref, inView, entry } = useInView({
     threshold: [0.2, 0.4],
-    rootMargin: "-450px 0px -300px 0px",
+    rootMargin: "-250px 0px -200px 0px",
 
     // trackVisibility: true,
-    // delay: 100,
+    // delay: 400,
     // rootMargin: "300px",
   });
 
   const useStyles = makeStyles({
-    loadingImg: {
-      background: "linear-gradient(270deg, #121212, #5a5b5b)",
-      backgroundSize: "400% 400%",
-      WebkitAnimation: "AnimationName 1s ease infinite",
-      MozAnimation: "AnimationName 1s ease infinite",
-      OAnimation: "AnimationName 1s ease infinite",
-      animation: "AnimationName 1s ease infinite",
-    },
-
-    "@-webkit-keyframes AnimationName": {
-      "0%": { backgroundPosition: "0% 50%" },
-      "50%": { backgroundPosition: "100% 50%" },
-      "100%": { backgroundPosition: "0% 50%" },
-    },
-    "@-moz-keyframes AnimationName": {
-      "0%": { backgroundPosition: "0% 50%" },
-      "50%": { backgroundPosition: "100% 50%" },
-      "100%": { backgroundPosition: "0% 50%" },
-    },
-    "@-o-keyframes AnimationName": {
-      "0%": { backgroundPosition: "0% 50%" },
-      "50%": { backgroundPosition: "100% 50%" },
-      "100%": { backgroundPosition: "0% 50%" },
-    },
-    "@keyframes AnimationName": {
-      "0%": { backgroundPosition: "0% 50%" },
-      "50%": { backgroundPosition: "100% 50%" },
-      "100%": { backgroundPosition: "0% 50%" },
-    },
+    // loadingImg: {
+    //   background: "linear-gradient(270deg, #121212, #5a5b5b)",
+    //   backgroundSize: "400% 400%",
+    //   WebkitAnimation: "AnimationName 1s ease infinite",
+    //   MozAnimation: "AnimationName 1s ease infinite",
+    //   OAnimation: "AnimationName 1s ease infinite",
+    //   animation: "AnimationName 1s ease infinite",
+    // },
+    // "@-webkit-keyframes AnimationName": {
+    //   "0%": { backgroundPosition: "0% 50%" },
+    //   "50%": { backgroundPosition: "100% 50%" },
+    //   "100%": { backgroundPosition: "0% 50%" },
+    // },
+    // "@-moz-keyframes AnimationName": {
+    //   "0%": { backgroundPosition: "0% 50%" },
+    //   "50%": { backgroundPosition: "100% 50%" },
+    //   "100%": { backgroundPosition: "0% 50%" },
+    // },
+    // "@-o-keyframes AnimationName": {
+    //   "0%": { backgroundPosition: "0% 50%" },
+    //   "50%": { backgroundPosition: "100% 50%" },
+    //   "100%": { backgroundPosition: "0% 50%" },
+    // },
+    // "@keyframes AnimationName": {
+    //   "0%": { backgroundPosition: "0% 50%" },
+    //   "50%": { backgroundPosition: "100% 50%" },
+    //   "100%": { backgroundPosition: "0% 50%" },
+    // },
   });
 
   const classes = useStyles();
 
   const variants = {
+    initialWrap: {
+      y: 140,
+    },
     appear: {
-      y: !hasShown || isXs ? 140 : 0,
+      y: 0,
       transition: {
         type: "anticipate",
         duration: 1,
@@ -80,6 +87,7 @@ const LazyProject = ({ width, height, src, ...rest }) => {
     if (!hasShown && inView) {
       setHasShown(true);
     }
+    setIsPlay(inView);
   }, [inView]);
 
   return (
@@ -90,48 +98,65 @@ const LazyProject = ({ width, height, src, ...rest }) => {
         display: "flex",
         justifyContent: "center",
         alignItems: "center",
-        marginBottom: !isXs ? "55px" : "10px",
-        minHeight: !isXs && "530px",
+        marginBottom: !isXs ? "150px" : "40px",
+        minHeight: "530px",
         flexDirection: "column",
       }}
       variants={variants}
-      initial={{ y: !hasShown ? 140 : 0 }}
-      animate={["appear", "changeBg"]}
+      animate={["changeBg"]}
     >
-      <div style={{ width: "100%" }}>
-        <Typography color="primary">
-          <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
-            work for COMPANY NAME
-          </motion.span>{" "}
-          <br />
-          <motion.span> Description of the work... ... ...</motion.span>
-        </Typography>
-      </div>
-      <div
-        data-inview={inView}
-        style={{
-          position: "relative",
-          minHeight: height / 1.2,
-          width: "100%",
-          maxWidth: "440px",
-        }}
-      >
-        {hasShown && (
-          <motion.img
+      {hasShown && (
+        <motion.div variants={variants} initial="initialWrap" animate="appear">
+          <div style={{ width: "100%" }}>
+            <Typography color="primary">
+              <motion.span initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+                work for COMPANY NAME
+              </motion.span>
+              <br />
+              <motion.span> Description of the work... ... ...</motion.span>
+            </Typography>
+          </div>
+          <motion.div
+            // data-inview={inView}
+            style={{
+              position: "relative",
+              minHeight: height / 1.2,
+              width: "100%",
+            }}
             initial={{ opacity: 0.2 }}
             animate={{ opacity: inView ? 1 : 0.1 }}
-            exit={{ opacity: 0 }}
-            transition={{ type: "anticipate", duration: 0.8 }}
-            {...rest}
-            src={src}
-            style={{
-              maxWidth: "440px",
-              width: "100%",
-              height: "auto",
-            }}
-          />
-        )}
-      </div>
+          >
+            <>
+              {/* <motion.img
+              
+              exit={{ opacity: 0 }}
+              transition={{ type: "anticipate", duration: 0.8 }}
+              {...rest}
+              src={src}
+              style={{
+                maxWidth: "440px",
+                width: "100%",
+                height: "auto",
+              }}
+            /> */}
+              {/* <motion.div
+              initial={{ opacity: 0.2 }}
+              animate={{ opacity: inView ? 1 : 0.1 }}
+            > */}
+              <ReactPlayer
+                url={ddbc}
+                playing={false}
+                controls
+                loop
+                muted
+                width="100%"
+                height="80%"
+              />
+              {/* </motion.div> */}
+            </>
+          </motion.div>
+        </motion.div>
+      )}
     </motion.div>
   );
 };
